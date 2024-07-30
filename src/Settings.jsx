b8c3a9432d67de96bc8d404c618785da382f6b94
James <rohithr.indigital@gmail.com>
2024-07-30 19:21:49 +0530

    settings fix

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -5,14 +5,20 @@ export default function Settings() {
                 <div class="modal-dialog modal-fullscreen">
                     <div class="modal-content bg-black text-light">
                         <div class="modal-header border-dark">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Settings</h1>
+                            <div className="container">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Settings</h1>
+                            </div>
                         </div>
                         <div class="modal-body border-dark">
-                            Settings coming soon
+                            <div className="container">
+                                Settings coming soon
+                            </div>
                         </div>
                         <div class="modal-footer border-dark">
-                            <button type="button" class="btn btn-dark border-0 rounded-1" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-dark border-0 rounded-1">Save changes</button>
+                            <div className="container d-flex align-items-center m-auto">
+                                <button type="button" class="btn btn-dark ms-auto me-2 border-0 rounded-1" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-dark border-0 rounded-1">Save changes</button>
+                            </div>
                         </div>
                     </div>
                 </div>
